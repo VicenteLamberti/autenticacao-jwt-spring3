6230cfca18e00e45e207ed3d107f01c261a59f4e
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-10-02 11:47:46 -0300

    Criando o filtro

---
+++ src/main/java/br/com/vicente/authjwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,37 @@
+package br.com.vicente.authjwt.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer"){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        jwtService.extractUsename(jwt);
+
+
+    }
+}
---
+++ src/main/java/br/com/vicente/authjwt/config/JwtService.java
@@ -0,0 +1,10 @@
+package br.com.vicente.authjwt.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsename(final String token){
+        return null;
+    }
+}
