2d7bbae18dbd1802a11e4d1ec9106ed2db6bd8c3
Vicente Leonardo De Souza Lamberti <vicente.lamberti@hotmail.com>
2023-10-02 09:05:03 -0300

    Criação do projeto

---
+++ src/main/java/br/com/vicente/authjwt/AuthjwtApplication.java
@@ -0,0 +1,13 @@
+package br.com.vicente.authjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthjwtApplication.class, args);
+	}
+
+}
